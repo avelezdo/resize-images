f6a311a7c417a09901f79f7039ba74f1a2ff0339
avelezdo <albertovelezdev@gmail.com>
2022-04-14 10:07:39 +0200

    Se cambia de ubicación el botón de eliminar tamaño. Actualizado el svg

--- src/components/DimensionList.jsx
+++ src/components/DimensionList.jsx
@@ -4,34 +4,60 @@ import Dimensions from './Dimensions'
 
 function DimensionList() {
 	const [dimensionsCounter, setDimensionsCounter] = useState(1)
-	const [dimensionList, setDimensionList] = useState([<Dimensions dimensionsCounter={dimensionsCounter} callback={removeDimensionsCallback}></Dimensions>])
+	const [dimensionList, setDimensionList] = useState([<Dimensions dimensionsCounter={dimensionsCounter} key={0}></Dimensions>])
 
-	function removeDimensionsCallback() {
-		setDimensionsCounter(dimensionsCounter)
-		setDimensionList(dimensionList)
+	const addDimensions = () => {
+		setDimensionsCounter((prevState) => prevState + 1)
+		setDimensionList((prevState) => {
+			const newDimensionList = [...prevState]
+			newDimensionList.push(<Dimensions dimensionsCounter={dimensionsCounter} key={dimensionsCounter}></Dimensions>)
+			return newDimensionList
+		})
 	}
 
-	const addDimensions = (_) => {
-		setDimensionsCounter(dimensionsCounter + 1)
-		setDimensionList(dimensionList.concat(<Dimensions dimensionsCounter={dimensionsCounter + 1} callback={removeDimensionsCallback}></Dimensions>))
+	const removeDimensions = () => {
+		setDimensionsCounter((prevState) => prevState - 1)
+		setDimensionList((prevState) => {
+			const newDimensionList = [...prevState]
+			newDimensionList.pop()
+			return newDimensionList
+		})
 	}
-
-	//se debería capturar el evento de removeDimensions que se produce en el hijo
 
 	return (
 		<div className='dimensions-wrapper'>
 			{dimensionList}
-			<button type='button' data-add-task-navigation-element='true' className='plus_add_button' onClick={addDimensions}>
-				<span className='icon_add' aria-hidden='true'>
-					<svg width='13' height='13'>
-						<path
-							d='M6 6V.5a.5.5 0 0 1 1 0V6h5.5a.5.5 0 1 1 0 1H7v5.5a.5.5 0 1 1-1 0V7H.5a.5.5 0 0 1 0-1H6z'
-							fill='currentColor'
-							fillRule='evenodd'></path>
-					</svg>
-				</span>
-				Añadir tamaño
-			</button>
+			<div className='flex justify-start gap-3 sizes-buttons-wrapper'>
+				<button type='button' className='plus_add_button' onClick={addDimensions}>
+					<span className='add-icon' aria-hidden='true'>
+						<svg width='13' height='13'>
+							<path
+								d='M6 6V.5a.5.5 0 0 1 1 0V6h5.5a.5.5 0 1 1 0 1H7v5.5a.5.5 0 1 1-1 0V7H.5a.5.5 0 0 1 0-1H6z'
+								fill='currentColor'
+								fillRule='evenodd'></path>
+						</svg>
+					</span>
+					Add size
+				</button>
+				{dimensionsCounter > 1 && <span className='font-thin self-center'> | </span>}
+				{dimensionsCounter > 1 && (
+					<button className='remove-size' onClick={removeDimensions}>
+						<span className='remove-icon'>
+							<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 13 13' width='13' height='13'>
+								<g id='minus-icon'>
+									<path
+										id='minus-icon'
+										d='m13 6.5c0 0.6-0.5 1.1-1.2 1.1h-10.7c-0.3 0-0.6-0.1-0.8-0.3-0.2-0.2-0.3-0.5-0.3-0.8 0-0.6 0.5-1.1 1.1-1.1h10.7c0.7 0 1.2 0.5 1.2 1.1z'
+										fill='currentColor'
+										fillRule='evenodd'
+									/>
+								</g>
+							</svg>
+						</span>
+						Remove size
+					</button>
+				)}
+			</div>
 		</div>
 	)
 }
--- src/components/Dimensions.jsx
+++ src/components/Dimensions.jsx
@@ -1,25 +1,6 @@
-function Dimensions({ dimensionsCounter, callback: removeDimensionsCallback }) {
+function Dimensions({ dimensionsCounter }) {
 	return (
 		<div className='sizes-wrapper'>
-			{dimensionsCounter > 1 && (
-				<button onClick={removeDimensionsCallback}>
-					<svg xmlns='http://www.w3.org/2000/svg' version='1.2' viewBox='0 0 13 13' width='13' height='13'>
-						<g id='Layer'>
-							<path
-								id='Layer'
-								fillRule='evenodd'
-								fill='currentColor'
-								d='m13 6.5c0 1.7-0.7 3.4-1.9 4.6-1.2 1.2-2.9 1.9-4.6 1.9-1.7 0-3.4-0.7-4.6-1.9-2.5-2.5-2.5-6.7 0-9.2 1.2-1.2 2.9-1.9 4.6-1.9 1.7 0 3.4 0.7 4.6 1.9 1.2 1.2 1.9 2.9 1.9 4.6zm-1.2 0c0-1.4-0.5-2.8-1.5-3.8-1-1-2.4-1.5-3.8-1.5-1.4 0-2.8 0.5-3.8 1.5-2 2.1-2 5.5 0 7.6 1 1 2.4 1.5 3.8 1.5 1.4 0 2.8-0.5 3.8-1.5 1-1 1.5-2.4 1.5-3.8z'
-							/>
-							<path
-								id='Layer'
-								fill='currentColor'
-								d='m10.6 6.5c0 0.3-0.2 0.6-0.6 0.6h-7c-0.4 0-0.6-0.3-0.6-0.6 0-0.3 0.2-0.6 0.6-0.6h7c0.4 0 0.6 0.3 0.6 0.6z'
-							/>
-						</g>
-					</svg>
-				</button>
-			)}
 			<div className='size'>
 				<label htmlFor={`width-${dimensionsCounter}`}>Width</label>
 				<div className='relative'>
